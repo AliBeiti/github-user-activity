9490b7321aaf102a6049c56eabaa37bbf5fd8106
Ali Beiti <ali.beitiaydenlou003@stud.fh-dortmund.de>
2025-08-27 16:37:13 +0200

    Added setup file.

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name='github-events-cli',
+    name='github-events',
     version='0.1',
-    py_modules=['github_events'],
+    py_modules=['app'],
     install_requires=['requests'],
     entry_points={
         'console_scripts': [
-            'github-events=github_events:main',
+            'github-events=app:main',
         ],
     },
 )
